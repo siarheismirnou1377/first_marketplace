4e298b7d04e4c2f99289f403d32082c9129a094b
siarheismirnou1377 <smirnovsergei1377@gmail.com>
2023-07-25 16:45:56 +0300

    change urls and views

--- marketplaces/urls.py
+++ marketplaces/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path("categories/", views.categories, name="categories"),
     # Страница всех товаров по категрии
     path('categories/<int:category_id>/', views.category, name='category'),
+    # Страница всех товаров
+    path('products/', views.products, name='products'),
+    # Страница товара
+    path('/<int:product_id>/', views.product, name='product'),
 ]
  
--- marketplaces/views.py
+++ marketplaces/views.py
@@ -18,8 +18,15 @@ def category(request, category_id):
     context = {'category': category, 'products': products}
     return render(request, 'marketplaces/category.html', context)
 
-# def products(request):
-#     """Выводит страницу со всеми продуктами"""
-#     products = Product.objects.order_by('name')
-#     context = {'products': products}
-#     return render(request, 'marketplaces/products.html', context)
+def products(request):
+    """Выводит страницу со всеми продуктами"""
+    products = Product.objects.order_by('name')
+    context = {'products': products}
+    return render(request, 'marketplaces/products.html', context)
+
+def product(request, product_id):
+    """Выводит страницу со всеми товарами по категории"""
+    product = Product.objects.get(id=product_id)
+    #products = category.product_set.order_by('-date_added')
+    context = {'product': product}
+    return render(request, 'marketplaces/product.html', context)
