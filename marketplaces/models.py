49eb585d9d2c2ad2f8da9bb1221be44d96c6e742
siarheismirnou1377 <smirnovsergei1377@gmail.com>
2023-07-25 15:20:13 +0300

    change models

--- marketplaces/models.py
+++ marketplaces/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        """Возвращает строковое значение представления модели"""
+        return self.name
+
+class Product(models.Model):
+    """Класс товара, который может заказать клиент"""
+    category = models.ForeignKey(Category, on_delete = models.PROTECT)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField("price")
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+       
+    class Meta:
+        verbose_name_plural = "Products"
+    
+    def __str__(self):
+        """Возвращает строковое значение представления модели"""
+        return self.name 
