060cd3f0253d2eae759ef502b13124106d44db54
siarheismirnou1377 <smirnovsergei1377@gmail.com>
2023-07-26 21:58:25 +0300

    create app cart

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -31,9 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #My apps
+    # My apps
     'marketplaces',
     'users',
+    'cart',
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+
+CART_SESSION_ID = 'cart'
