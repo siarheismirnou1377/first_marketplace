eef720b9e956c4213268364d70963fa4d5bb793b
siarheismirnou1377 <smirnovsergei1377@gmail.com>
2023-07-26 22:25:55 +0300

    change cart_add into views

--- cart/views.py
+++ cart/views.py
@@ -16,7 +16,7 @@ def cart_add(request, product_id):
         cart.add(product=product,
                  quantity=cd['quantity'],
                  update_quantity=cd['update'])
-    return redirect('cart:cart_detail')
+    return redirect('marketplaces:products')
 
 def cart_remove(request, product_id):
     cart = Cart(request)
