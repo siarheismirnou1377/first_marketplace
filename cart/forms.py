bdfd99cf9a9e5cb7cd1f263dc200f7a9a7729e90
siarheismirnou1377 <smirnovsergei1377@gmail.com>
2023-07-28 23:35:36 +0300

    refactoring

--- cart/forms.py
+++ cart/forms.py
@@ -4,5 +4,6 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
+    """Форма, которая используется для добавления продуктов в корзину"""
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
