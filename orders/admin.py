8d5b07a723dac935ff45052cc51ba2e710a2c474
siarheismirnou1377 <smirnovsergei1377@gmail.com>
2023-07-29 17:32:42 +0300

    change OrderAdmin

--- orders/admin.py
+++ orders/admin.py
@@ -10,7 +10,7 @@ class OrderItemInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     """Класс отображаения информации по заказу"""
-    list_display = ['id', 'first_name', 'last_name', 'email',
+    list_display = ['id','first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
