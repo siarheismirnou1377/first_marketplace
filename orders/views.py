4825478f24f7af31af36b6c8706b6e51f563e19b
siarheismirnou1377 <smirnovsergei1377@gmail.com>
2023-07-29 19:18:18 +0300

    add func orders

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from django.contrib.auth.decorators import login_required
+
 
 
 def order_create(request):
@@ -25,3 +27,10 @@ def order_create(request):
         form = OrderCreateForm
     return render(request, 'orders/order/create_order.html',
                   {'cart': cart, 'form': form})
+
+@login_required
+def orders(request):
+    """Выводит страницу со всеми заказами пользователя"""
+    orders_user = OrderItem.objects.filter(owner=request.user).all()
+    context = {'orders': orders_user}
+    return render(request, 'orders/order/orders.html', context)
